78558eaab9ff47f7eb4c5c243eb7fce3c6efe126
azeezabidoye <azeezabidoye@yahoo.com>
2024-07-04 16:58:16 +0100

    Hardhat config setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("solidity-coverage");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("dote");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
